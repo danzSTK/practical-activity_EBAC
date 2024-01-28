0829b9e8002e22fd076d70ab92ebbdeb28aa3ad1
Carlos Daniel <carlosdanielfelixfarias@gmail.com>
2024-01-28 15:11:43 -0300

    fix: removendo quebra de linha

--- lista_de_tarefas/script.js
+++ lista_de_tarefas/script.js
@@ -1,14 +1,10 @@
 const form = document.getElementById('form');
-
-
-
 $(document).ready(function(){
     $(form).submit(function(e){
         e.preventDefault()
 
         const nomeAtividade = $('#nome-tarefa').val()
         $(`<li><button onclick="addTask(this)" type="submit">${nomeAtividade}</button></li>`).appendTo('#tarefa-lista')
-    
     })
 })
 
