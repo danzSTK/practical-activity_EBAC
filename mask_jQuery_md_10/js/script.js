70a545612e3b341e329c8a4466fda7c97a44f05e
Carlos Daniel <carlosdanielfelixfarias@gmail.com>
2024-03-27 13:16:29 -0300

    docs:removendo pastas

--- mask_jQuery_md_10/js/script.js
+++
@@ -1,9 +0,0 @@
-$('#phone').mask('(00) 00000-0000');
-$('#cep').mask('00000-000');
-$('#cpf').mask('000.000.000-00');
-
-$('form').submit(function(e){
-    e.preventDefault();
-
-    $('input').val('')
-})
