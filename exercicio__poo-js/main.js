77e9f675ce336ab27ad1ff866e04b19993ebd4d0
Daniel_Félix <carlosdanielfelixfarias@gmail.com>
2024-06-24 14:30:00 -0300

    adicionando function dev

--- exercicio__poo-js/main.js
+++ exercicio__poo-js/main.js
@@ -18,14 +18,29 @@ function Professor(nome, idade, salario){
 }
 
 
+function Dev(nome, idade, cargo, salario){
+    this.cargo = cargo
+
+
+    this.apresentaDev = () =>{
+        console.log('Olá, meu nome é ' + this.nome + ' e tenho ' + this.idade +  ' anos. Sou ' + this.cargo + ' e recebo: R$' + salario)
+    }
+
+    Professor.call(this, salario)
+    Aluno.call(this, nome, idade)
+}
+
+
 const professor = new Professor('Daniel', 27, 2500)
 const aluno = new Aluno('Matheus', 16)
+const dev = new Dev('Carlos', 19, 'Desenvolvedor Front-end', 3000)
 
 
 function apresentacao(){
     console.log(professor.apresentaProfessor())
     console.log(aluno.apresentaAluno())
-    console.log('Fazemos parte do colégio Acadêmico')
+    console.log(dev.apresentaDev())
+
 }
 
 apresentacao()
