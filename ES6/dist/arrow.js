b9aab9d9563eb380c0fd9534dd1865441607dbdb
Daniel_Félix <carlosdanielfelixfarias@gmail.com>
2024-07-16 19:56:32 -0300

    Adicionando novas praticas

---
+++ ES6/dist/arrow.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var minhaFuncao = function minhaFuncao() {
+  return 'diz olá';
+};
+var retornaUmCarro = function retornaUmCarro() {
+  return {
+    modelo: 'Ka',
+    fabricante: 'Ford'
+  };
+};
+console.log(minhaFuncao());
