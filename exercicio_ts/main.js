cfe8c04eef937bb10397def6e1ac322e3e06bfb1
Daniel_Félix <carlosdanielfelixfarias@gmail.com>
2024-07-19 04:19:58 -0300

    conclusão do exercicio 1.0

---
+++ exercicio_ts/main.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicador(valor1, valor2) {
+    return valor1 * valor2;
+}
+function dizOi(nome) {
+    return `Olá ${nome}`;
+}
+console.log(multiplicador(10, 10));
+console.log(dizOi('Ebac'));
---
+++ exercicio_ts/main.ts
@@ -0,0 +1,11 @@
+function multiplicador(valor1:number, valor2: number):number{
+    return valor1 * valor2
+}
+
+function dizOi(nome:string):string{
+    return `Olá ${nome}`
+}
+
+
+console.log(multiplicador(10, 10));
+console.log(dizOi('Ebac'))
