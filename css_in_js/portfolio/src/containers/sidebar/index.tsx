8ccad2d62cc3bbdb8c6a794c46d4c414b9c36022
Daniel_Félix <carlosdanielfelixfarias@gmail.com>
2024-08-14 13:02:28 -0300

    estilizando a sidebar

--- css_in_js/portfolio/src/containers/sidebar/index.tsx
+++ css_in_js/portfolio/src/containers/sidebar/index.tsx
@@ -1,10 +1,31 @@
+import styled from 'styled-components'
 import Title from '../../components/Title'
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
+import { Descricao, BotaoTema, SiderbarContainer } from './styles'
+
+const Aside = styled.aside`
+  h3 {
+    display: block;
+    text-align: center;
+    margin-top: 16px;
+  }
+`
 
 const Sidebar = () => (
-  <aside>
-    <img src="https://github.com/danzSTK.png" />
-    <Title fontSize={16}>Daniel Félix</Title>
-  </aside>
+  <Aside>
+    <SiderbarContainer>
+      <Avatar />
+      <Title fontSize={20}>Daniel Félix</Title>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        danzSTK
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema type="button">Trocar Tema</BotaoTema>
+    </SiderbarContainer>
+  </Aside>
 )
 
 export default Sidebar
