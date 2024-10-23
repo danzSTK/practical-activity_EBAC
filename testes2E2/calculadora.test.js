a3624bf8323ce9c5b370d8dffd7aabbced93fe6f
Daniel_Félix <carlosdanielfelixfarias@gmail.com>
2024-10-23 09:29:00 -0300

    adicionando E2E

---
+++ testes2E2/calculadora.test.js
@@ -0,0 +1,16 @@
+const calc = require('./calculadora')
+
+// describe é um switch de teste 
+describe('Teste para a calculadora', () => {
+    test('somar 2 e 2 deve resultar em 4', () => {
+        const minhaSoma = calc.soma(2, 2)
+    
+        expect(minhaSoma).toBe(4)
+    })
+    
+    test('multiplicar 3 com 3 deve resultar em 9', () => {
+        const minhaMultiplicacao = calc.multiplicacao(3, 3)
+    
+        expect(minhaMultiplicacao).toBe(9)
+    })
+})
